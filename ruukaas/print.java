d9b9a27edc979c227cc5a728d7173d9831d4fb4c
Ruukaas <lucashenriquehca@gmail.com>
2022-06-06 00:21:38 -0300

    Lucas Henrique Costa de Arruda(@ruukaas)

---
+++ ruukaas/print.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Imprimindo msg");
+        System.out.println("ruukaas");
+        System.out.println("Parte 4");
+        System.out.println("Pratica 3");
+    }
+}
