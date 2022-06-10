90dd0d2caf1b2bf15def43109a7b7f90b21ba59c
Davidson Felix da Silva <davidson@MacBook-Pro-de-Davidson.local>
2022-06-10 11:35:37 -0300

    Primeiro commit - pratica03_GCMS

---
+++ src/br/com/ifpe/HelloWold.java
@@ -0,0 +1,11 @@
+package br.com.ifpe;
+
+public class HelloWold {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Wold!!");
+
+        System.out.println("Primeiro commit - Teste_pratica03");
+    }
+}
