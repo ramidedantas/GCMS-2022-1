b59f40d7da9dbf52215a6c02a3b3832999d83696
Laercio <laercio.rodrigs@gmail.com>
2022-08-05 01:16:04 -0300

    commit branch laercio

--- savate/HelloWorld.java
+++ savate/HelloWorld.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 	System.out.println("Laércio Rodrigues");
+	System.out.println("Na branch laercio");
+	System.out.println("Hello, World na branch laercio");
     }
 }
