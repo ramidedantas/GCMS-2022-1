f97223aee5734f016f30b2d6eb148a90c614f17d
José Alfredo <netoalfredo9@gmail.com>
2022-08-11 03:15:00 -0300

    Pull Request[1] - José Alfredo

--- Git-Aula-2022-1/src/HelloWorld.java
+++ Git-Aula-2022-1/src/HelloWorld.java
@@ -32,6 +32,7 @@ public class HelloWorld {
 		System.out.println("Pull Request Lucas Arruda 01/08");
 		System.out.println("modificação de Igor Cabral para a parte 04, passo 02, da prática 03 em 10/08");
 		System.out.println("Commit identificando a PT3 - Passo 2 - Pratica 04 (José Alfredo)");
+		System.out.println("Trabalhando com Pull Requests [1] - (José Alfredo)");
 	}
 
 }
