ca1c880104f3315a09e3fc1f1eaa463ebcac2cb5
JorgeLucasIFPE <jlld@discente.ifpe.edu.br>
2022-08-17 11:26:43 -0300

    Adding my file

---
+++ jlucas/print.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("printing some msg");
+        System.out.println("made by Jorge lucas");
+        System.out.println("Step 4");
+        System.out.println(" 3");
+
+         System.out.println("Practice 4 pull request");
+         System.out.println("Practice 4 push request");
+
+    }
+}
