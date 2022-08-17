32b524b570ace5bebb9ac4754bc77e1048c6f628
JorgeLucasIFPE <jlld@discente.ifpe.edu.br>
2022-08-17 11:54:06 -0300

    Now

--- jlucas/print.java
+++ jlucas/print.java
@@ -4,7 +4,9 @@ public class Main {
         System.out.println("made by Jorge lucas");
         System.out.println("Step 4");
         System.out.println(" Practice 3");
+
         System.out.println("Correcting branch");
+        System.out.println("Now!");
 
         System.out.println("Practice 4 step 4 push request");
 
