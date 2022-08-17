4022e419a57a59b2d15783ab5f98ba4c0953e018
JorgeLucasIFPE <jlld@discente.ifpe.edu.br>
2022-08-17 11:50:09 -0300

    Fork push

--- jlucas/print.java
+++ jlucas/print.java
@@ -3,10 +3,9 @@ public class Main {
         System.out.println("printing some msg");
         System.out.println("made by Jorge lucas");
         System.out.println("Step 4");
-        System.out.println(" 3");
+        System.out.println(" Practice 3");
 
-         System.out.println("Practice 4 pull request");
-         System.out.println("Practice 4 push request");
+        System.out.println("Practice 4 step 4 push request");
 
     }
 }
